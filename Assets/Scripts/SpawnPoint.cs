43ec0d90c2925f71e968ed6107545fb56b95a2f7
Ra-eez <eezy1997@gmail.com>
2022-07-28 00:50:30 +0200

    Removed old scenes, good progress on lvl 1, starting lvl 3

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject[] spawnLocations;
+    public GameObject[] items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       for (int i = 0; i<items.Length; i++)
+        {
+
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
